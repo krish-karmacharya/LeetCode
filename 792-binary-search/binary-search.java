84df5abac1c3e524fe1114167443c94507eb5283
krish-karmacharya <110962590+krish-karmacharya@users.noreply.github.com>
2024-04-06 11:13:33 +0545

    Time: 0 ms (100.00%) | Memory: 45.7 MB (44.63%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -4,7 +4,6 @@ class Solution {
     public int search(int[] arr, int target) {
        int start =0;
        int end =arr.length -1;
-
        while(start <=end){
         //find the middle element
         // int mid =(start + end )/2;
@@ -12,7 +11,6 @@ class Solution {
         int mid = start + (end - start) / 2;
         if(target < arr[mid]){
             end = mid - 1;
-
         }else if(target>arr[mid]){
             start = mid +1;
         }else{
